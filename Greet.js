e5222a8d7affad57bb04f306dca853bd235fdd34
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-29 18:22:15 -0700

    Update Greet.js

--- Greet.js
+++ Greet.js
@@ -14,3 +14,9 @@ Example:
 let greet = function(name) {
   return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
 }
+
+// or
+
+let greeting = function(name) {
+  return `Hello ${name.slice(0,1).toUpperCase()}${name.slice(1).toLowerCase()}!`;
+}
